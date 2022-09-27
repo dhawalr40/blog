648454030531537608d540ade30da0bda1a7f4f0
dhawalr40 <dhawalr40@gmail.com>
2022-09-27 11:48:57 +0530

    week2 80%

--- Gemfile
+++ Gemfile
@@ -50,6 +50,9 @@ gem "bootsnap", require: false
 
 gem 'devise', '~> 4.8', '>= 4.8.1'
 
+gem "rolify"
+gem 'awesome_print', '~> 1.8'
+
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem "debug", platforms: %i[ mri mingw x64_mingw ]
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,15 @@
 class User < ApplicationRecord
+  rolify
+  # resourcify
 
   has_many :articles, dependent: :destroy
   has_many :likes, dependent: :destroy
+
+  after_create :assign_default_role
+
+  def assign_default_role
+    self.add_role(:newuser) if self.roles.blank?
+  end
 
   # before_action :authenticate_user
   # Include default devise modules. Others available are:
